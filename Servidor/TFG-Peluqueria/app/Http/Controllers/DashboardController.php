05e29a64ff5e3ad23136614b5949a2d960e76ad6
nnavrio140 <nnavrio140@g.educaand.es>
2026-02-13 12:58:05 +0100

    Limpiado de dashboardController

--- Servidor/TFG-Peluqueria/app/Http/Controllers/DashboardController.php
+++ Servidor/TFG-Peluqueria/app/Http/Controllers/DashboardController.php
@@ -11,30 +11,14 @@ use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        // Obtener usuario logado (garantizado por middleware auth)
-        /** @var User $usuario */
+   public function index() {
+    
         $usuario = Auth::user();
 
-        // Obtener citas segun roles
-        if ($usuario->isAdmin()) {
-            //Si es admin obtener todas las citas del día actual
-            $citas = Cita::with(['usuario', 'servicio', 'empleado', 'estado'])->whereDate('fecha', Carbon::today())->get();
-        } elseif ($usuario->isEmployee()) {
-            // Si es empleado obtener todas las citas del día actual suyas
-            $citas = Cita::with(['usuario', 'servicio', 'empleado', 'estado'])->whereDate('fecha', Carbon::today())->where('id_empleado', $usuario->empleado->id)->get();
-        } else {
-            // Si es cliente obtener todas sus citas da igual el día
-            $citas = Cita::with(['usuario', 'servicio', 'empleado', 'estado'])->where('user_id', $usuario->id)->get();
-        }
-        // Obtener todos los servicios
+        $citas = Cita::paraDashboard($usuario)->get();
+
         $servicios = Servicio::all();
-
-         // Obtener todos los usuarios
         $usuarios = User::all();
-
-        // Obtener todos los estados
         $estados = Estado::all();
 
         return view('dashboard.index', compact('usuario', 'citas', 'servicios', 'estados', 'usuarios'));
--- Servidor/TFG-Peluqueria/app/Models/Cita.php
+++ Servidor/TFG-Peluqueria/app/Models/Cita.php
@@ -9,6 +9,8 @@ use App\Models\Servicio;
 use App\Models\Empleado;
 use App\Models\Estado;
 use App\Models\HistorialCita;
+use Carbon\Carbon;
+use App\Models\User;
 
 
 class Cita extends Model
@@ -50,5 +52,22 @@ class Cita extends Model
     public function historial()
     {
         return $this->hasMany(HistorialCita::class, 'id_cita');
+    }
+
+    // Scope para filtrar citas por fecha y usuario es necesario que se llame scope 
+    public function scopeParaDashboard($query, User $usuario)
+    {
+        $query->with(['usuario', 'servicio', 'empleado', 'estado']);
+
+        if ($usuario->isAdmin()) {
+            return $query->whereDate('fecha', Carbon::today());
+        }
+
+        if ($usuario->isEmployee()) {
+            return $query->whereDate('fecha', Carbon::today())
+                        ->where('id_empleado', $usuario->empleado->id);
+        }
+
+        return $query->where('user_id', $usuario->id);
     }
 }
