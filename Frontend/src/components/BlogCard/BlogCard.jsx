3abe58196a1655cc3e6244ae682558ac9fa876cd
nnavrio140 <nnavrio140@g.educaand.es>
2026-05-21 11:30:37 +0200

    2 intneto blog imagenes

--- Frontend/src/components/BlogCard/BlogCard.jsx
+++ Frontend/src/components/BlogCard/BlogCard.jsx
@@ -5,16 +5,14 @@ function BlogCard({ post }) {
     <div className="blog-card">
       <div className="blog-card__image-container">
         <img
-          src={post.image_url || "/img/default.webp"}
+          src={post.image || "/img/default.webp"}
           alt={post.title}
           className="blog-card__image"
           loading="lazy"
         />
       </div>
 
-      <h3 className="blog-card__title">
-        {post.title}
-      </h3>
+      <h3 className="blog-card__title">{post.title}</h3>
     </div>
   );
 }
